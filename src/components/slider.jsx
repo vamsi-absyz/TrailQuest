0d5c7e75b8c4f87e1e27012d065215a700ff0673
DivyaV <saran.senguttuvan@absyz.com>
2024-09-17 12:28:21 +0530

    fixed the responsiveness

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,52 +1,3 @@
-// import { useEffect, useState } from "react";
-// import "./Slider.css"; // Import the CSS for animations
-// import Astro from "../assets/images/Astro.jpg";
-// import Bobcat from "../assets/images/Bobcat.jpg";
-// import Brandy from "../assets/images/Brandy.jpg";
-// import Cloudy from "../assets/images/Cloudy.jpg";
-// import Codey from "../assets/images/Codey.jpg";
-// import Dog from "../assets/images/Dog.jpg";
-// import Einstien from "../assets/images/Einstien.jpg";
-// import MaxTheMule from "../assets/images/Max-the-mule.jpg";
-
-// const images = [
-//   Astro,
-//   Bobcat,
-//   Brandy,
-//   Cloudy,
-//   Codey,
-//   Dog,
-//   Einstien,
-//   MaxTheMule,
-// ];
-
-// const Slider = () => {
-//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-//     }, 1200); // Change image every 1.2 milli seconds
-
-//     return () => clearInterval(interval); // Clean up on component unmount
-//   }, []);
-
-//   return (
-//     <div className="slider-container">
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           className={`image-box ${currentImageIndex === index ? "active" : ""}`}
-//         >
-//           <img src={image} alt={`Slide ${index}`} className="slider-image" />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Slider;
-
 import { useEffect, useState } from "react";
 import "./Slider.css"; // Import the CSS for animations
 import Sassy from "../assets/images/Sassy.jpg";
@@ -89,7 +40,7 @@ const Slider = () => {
   }, []);
 
   return (
-    <div className="slider-container">
+    <div className="slider-container w-[100%] h-[275px] sm:h-[325px]  lg:w-[100%] lg:h-[400px]">
       {images.map((image, index) => (
         <div
           key={index}
@@ -107,7 +58,7 @@ const Slider = () => {
             }`}
           style={{ justifyContent: image.pos === "right" ? "end" : "start" }}
         >
-          <img src={image.img} alt={`Slide ${index}`} className="slider-image" />
+          <img src={image.img} alt={`Slide ${index}`} className="slider-image w-[100%] lg:w-[250px] lg:h-[350px] " />
         </div>
       ))}
     </div>
