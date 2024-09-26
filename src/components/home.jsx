99074d5711e6db474c690ffd7b0ce6e7a83aa284
DivyaV <saran.senguttuvan@absyz.com>
2024-09-26 16:54:22 +0530

    changed the grid value in home component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -102,13 +102,13 @@ export const Home = () => {
           spacing={2}
           className="m-auto h-full w-full flex justify-center"
         >
-          <Grid item md={0.5}></Grid>
+          <Grid item xs={0} md={0.5}></Grid>
 
           {/* Chip Group */}
           <Grid
             item
             xs={10}
-            sm={10}
+            sm={7}
             md={5.5}
             // lg={5.5}
             className="flex justify-start items-start flex-wrap !flex-col mt-0 lg:!mt-[60px] "
@@ -153,6 +153,7 @@ export const Home = () => {
               </Group>
             </Chip.Group>
           </Grid>
+          
 
           {/* Carousel section */}
           <Grid
