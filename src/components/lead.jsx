276d32245f2aa9cf35f1c9abad531da8f0a018c1
DivyaV <saran.senguttuvan@absyz.com>
2024-10-01 15:30:54 +0530

    made changes in the absyz logo

--- src/components/lead.jsx
+++ src/components/lead.jsx
@@ -84,7 +84,7 @@ export const Lead = () => {
       sx={{ flexGrow: 1 }}
       className="bg-[#F2F3F3] bg-img w-full flex flex-col justify-start items-center bg-cover md:bg-contain"
     >
-      <div className="flex lg:hidden w-full justify-center items-center">
+      <div className="flex lg:hidden w-full justify-center items-center mt-[1rem]">
         <img src={Logo} alt="logo" className="w-[110px] " />
       </div>
       <div className="w-full sm:px-[2rem] md:px-[2rem] sm:pb-[2rem] md:pb-1 pt-[10px] ">
