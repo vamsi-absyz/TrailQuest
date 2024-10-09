ddf2338949f5c27245838d386f48c89a90b8a12b
Sandeepsai-Pucha <sandeep.pucha@absyz.com>
2024-10-09 11:47:13 +0530

    TEsting 123

--- src/components/lead.jsx
+++ src/components/lead.jsx
@@ -114,7 +114,7 @@ export const Lead = () => {
           >
             <div className="mb-[8px]">
               <span className="font-medium !text-[22px] text-[#17233A] sm:!text-[26px] md:!text-[22px] lg:!text-[22px] ">
-                Let’s Make Things Happen Together!
+                Things Happen Together!
               </span>
             </div>
             <div className="w-full ">
